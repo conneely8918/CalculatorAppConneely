bbb8db26eddf2605e2bcbbcff67ffbac7c072d13
conneely8918 <113694725+conneely8918@users.noreply.github.com>
2023-09-09 19:58:11 -0500

    UI Updates and Java 'transitions'
    -Updated the UI, *almost* done
    -Utilized Class demo "one method, many uses" for the number buttons
    -TODO: Get button input to actually show up on the TextView.
    -TODO: Recognize Button Input for the  operations
    -TODO: Make the operations work

--- app/src/main/java/com/example/calculatorappconneely/MainActivity.java
+++ app/src/main/java/com/example/calculatorappconneely/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.calculatorappconneely;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private int numInput;
+    private int shownInput;
+    public static void main (String[]args){
+
+    }
+    /** The function numSelected differentiate between various button inputs.
+     * @param v the screen view
+     * Source: Denna "One Method, Many Uses" Example
+     */
+    public void numSelected(View v) {
+        if(v.getId() == R.id.buttonFor1) {
+            Log.i("User", "Selected 1");
+            numInput = 1;
+        } else if(v.getId() == R.id.buttonFor2) {
+            Log.i("User", "Selected 2");
+            numInput = 2;
+        } else if(v.getId() == R.id.buttonFor3) {
+            Log.i("User", "Selected 3");
+            numInput = 3;
+        } else if(v.getId() == R.id.buttonFor4) {
+            Log.i("User", "Selected 4");
+            numInput = 4;
+        } else if(v.getId() == R.id.buttonFor5) {
+            Log.i("User", "Selected 5");
+            numInput = 5;
+        }else if(v.getId() == R.id.buttonFor6) {
+            Log.i("User", "Selected 6");
+            numInput = 6;
+        } else if(v.getId() == R.id.buttonFor7) {
+            Log.i("User", "Selected 7");
+            numInput = 7;
+        }else if(v.getId() == R.id.buttonFor8) {
+            Log.i("User", "Selected 8");
+            numInput = 8;
+        } else if(v.getId() == R.id.buttonFor9) {
+            Log.i("User", "Selected 9");
+            numInput = 9;
+        } else if(v.getId() == R.id.buttonFor0) {
+            Log.i("User", "Selected 0");
+            numInput = 0;
+        } else{
+            Log.i("User","Selected A_Button_Without_Functionality");
+        }
+        shownInput *= 10;
+        shownInput += numInput;
+        Log.i("User", "Input "+shownInput);
+
+        String UI_Input = String.valueOf(shownInput);
+        
+
+        // make a reference to the TextView by locating it with its id
+        TextView UI_OutputTV = findViewById(R.id.UIOutputTextView);
+        String UI_Output = UI_OutputTV.getText().toString(); //xml:UIOutput_TextView, java: UI_OutputTV, str: UI_output
+        int output = Integer.parseInt(UI_Output);
     }
 }
